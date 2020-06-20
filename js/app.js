895a85b1a84733fd3fd54a48cf4b0f340784d612
Ahmed Hamed <tunashark@gmail.com>
2020-06-20 16:07:06 +0100

    adding event listeners to fields

--- js/app.js
+++ js/app.js
@@ -1,23 +1,25 @@
-// const RemoveNode = require('./removetest.js');
-
 document.addEventListener('DOMContentLoaded', () => {
-    // console.log('JavaScipt has loaded');    
+
     const destinyOneExpansionsNumber = document.querySelector('#destiny-1-expansion-fields');
     destinyOneExpansionsNumber.addEventListener('click', destinyExpansionsAddFields);
+    
+    // const destinyOneExpansionsField = document.querySelector('#destiny-one-field-0');
+    // destinyOneExpansionsField.addEventListener('input', destinyOneFieldEntry);
 })
 
-let counter = 0;
+let destinyOneFieldsCounter = 0;
 
-const destinyExpansionsAddFields = function () {
+const destinyExpansionsAddFields = function (clickEvent) {
     const destinyOneDiv = document.querySelector('#destiny-1-expansions');
     
-    if (counter < 3) {
-
+    if (destinyOneFieldsCounter < 4) {
+        //adding the new field and pushing it to the div container
         const destinyOneExpansionField = document.createElement('input');
         destinyOneExpansionField.setAttribute('type', Text)
-        destinyOneExpansionField.setAttribute('id', 'destiny-one-field' + counter)
+        destinyOneExpansionField.setAttribute('id', 'destiny-one-field' + '-' + destinyOneFieldsCounter)
         destinyOneDiv.appendChild(destinyOneExpansionField);
 
+        //adding the remove-field link, to remove the newly created field above
         const detinyOneExpansionDelete = document.createElement('a');
         detinyOneExpansionDelete.textContent = 'remove field';
         detinyOneExpansionDelete.href = "";
@@ -27,13 +29,18 @@ const destinyExpansionsAddFields = function () {
             detinyOneExpansionDelete.parentNode.removeChild(detinyOneExpansionDelete);
         }
 
-        
+        //adding the anchor after the newly created field, using nextSibling.
         destinyOneDiv.insertBefore(detinyOneExpansionDelete, destinyOneExpansionField.nextSibling)
 
-        counter++;
+        destinyOneFieldsCounter++;
     }
     else {
-        alert('Can\'t add more fields, sorry');
+        alert('Destiny 1 only had 4 expansions, nerd.');
+        clickEvent.reset();
     }
     
 }
+
+// const destinyOneFieldEntry = function (event) {
+
+// }
