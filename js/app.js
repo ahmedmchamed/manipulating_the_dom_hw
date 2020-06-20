a756c706736fe2de08d3af82b1e586fea888bbea
Ahmed Hamed <tunashark@gmail.com>
2020-06-20 13:55:01 +0100

    trying to add more fields

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('JavaScipt has loaded');
+    // console.log('JavaScipt has loaded');    
+    const destinyOneExpansionsNumber = document.querySelector('#destiny-1-expansion-fields');
+    destinyOneExpansionsNumber.addEventListener('click', destinyExpansionsAddFields);
 })
+
+// const removeCreatedField = function (elementId) {
+    
+//     removeElementLink = document.createElement('a');
+
+// }
+
+
+let counter = 0;
+
+const destinyExpansionsAddFields = function () {
+    const destinyOneDiv = document.querySelector('#destiny-1-expansions');
+
+    // let destinyOneHtmlInput = '<input type="text">' + ' ' + '<a href="/something">click here</a>'
+    // destinyOneDiv.innerHTML += destinyOneHtmlInput;
+    // let destinyOneExpansionNodes = document.querySelectorAll('')
+    if (counter < 5) {
+        const destinyOneExpansionField = document.createElement('input');
+        destinyOneExpansionField.setAttribute('type', Text)
+        destinyOneExpansionField.setAttribute('id', 'destiny-one-field' + counter)
+
+        destinyOneDiv.appendChild(destinyOneExpansionField);
+
+        counter++;
+    }
+    else {
+        alert('Can\'t add more fields, sorry');
+    }
+    
+}
